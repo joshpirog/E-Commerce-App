ee0ec9ce0d662b0316e7745940af591904d26ce2
Josh Pirog <josh.pirog@gmail.com>
2023-06-19 22:41:12 -0400

    project structure for users

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 require('dotenv').config()
+
+const loaders = require('./loaders');
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function startServer() {
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  })
+  
+  loaders(app);
+  
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
+}
+
+startServer();
--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
-const { DB } = require('../config');
+//const { DB } = require('../config');
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    // user: process.env.PG,
+    // host: process.env.PGHOST,
+    // database: process.env.PGDATABASE,
+    // password: process.env.PGPASSWORD,
+    // port: process.env.PGPORT
 });
 
 module.exports = {
